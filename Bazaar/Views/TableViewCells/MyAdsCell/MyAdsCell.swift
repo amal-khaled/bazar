a6709e580581d353259ee3ed4dfffce036b031ca
iOSayedAhmed <60572628+iOSayedAhmed@users.noreply.github.com>
2023-03-23 13:24:11 +0200

    edit my ads cell view (Design)

--- Bazaar/Views/TableViewCells/MyAdsCell/MyAdsCell.swift
+++ Bazaar/Views/TableViewCells/MyAdsCell/MyAdsCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MOLH
 
 class MyAdsCell: UITableViewCell {
     
@@ -15,6 +16,14 @@ class MyAdsCell: UITableViewCell {
     @IBOutlet weak var adImg: UIImageView!
     @IBOutlet weak var adName: UILabel!
     @IBOutlet weak var adPrice: UILabel!
+    @IBOutlet weak var adsLocation: UILabel!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var adsTime: UILabel!
+    @IBOutlet weak var editBtn: UIButton!
+    @IBOutlet weak var deleteBtn: UIButton!
+    @IBOutlet weak var shareBtn: UIButton!
+    
+    
     
     
     override func awakeFromNib() {
@@ -24,5 +33,18 @@ class MyAdsCell: UITableViewCell {
     
     func setupView() {
         adImg.addCornerRadius(cornerRadius: 20)
+        guard let editImage = UIImage(named: "editBtnIcon") else {return}
+        guard let deleteImage = UIImage(named: "deleteBtnIcon") else {return}
+        guard let shareImage = UIImage(named: "shareBtnIcon") else {return}
+        if MOLHLanguage.currentAppleLanguage() == "ar" {
+            editBtn.rightIcon(image: editImage)
+            deleteBtn.rightIcon(image: deleteImage)
+            shareBtn.rightIcon(image: shareImage)
+        }else{
+            editBtn.leftIcon(image: editImage)
+            deleteBtn.leftIcon(image: deleteImage)
+            shareBtn.leftIcon(image: shareImage)
+        }
+        
     }
 }
